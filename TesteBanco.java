8a7edd359e17757167b9d69c231326713cc5c5c3
Loyde Dourado | HTML | CSS | Javascript | Java | Spring Boot <140419686+loydesousa@users.noreply.github.com>
2025-04-19 20:41:10 -0300

    chore: cherry-pick of Loyde's commit from refactor branch

--- TesteBanco.java
+++ TesteBanco.java
@@ -18,6 +18,7 @@ public class TesteBanco {
         frame.setLayout(new FlowLayout());
 
         // Componentes da interface gráfica
+<<<<<<< HEAD
 
         // Componentes Corrente
         JButton TransferirPoupancaCorrenteButton = new JButton("Transferir");
@@ -29,19 +30,45 @@ public class TesteBanco {
 
         JButton depositPoupancaButton = new JButton("Depositar");
         JButton withdrawPoupancaButton = new JButton("Sacar");
+=======
+        JButton depositButton = new JButton("Depositar");
+        JButton depositPoupancaButton = new JButton("Depositar na Poupança");
+        JButton withdrawButton = new JButton("Sacar");
+        JButton withdrawPoupancaButton = new JButton("Sacar da Conta Poupança");
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
         JButton rendimentoButton = new JButton("Calcular Rendimento");
+        JButton TransferirCorrentePoupancaButton = new JButton("Transferência Corrente -> Poupança");
+        JButton TransferirPoupancaCorrenteButton = new JButton("Transferência Poupança -> Corrente");
 
+<<<<<<< HEAD
         JTextField amountCorrenteField = new JTextField(10);
         JTextField amountPoupancaField = new JTextField(10);
         JLabel balanceLabelCorrente = new JLabel("Saldo Conta Corrente: " + contaCorrente.getSaldo());
         JLabel balanceLabelPoupanca = new JLabel("Saldo Conta Poupança: " + contaPoupanca.getSaldo());
+=======
+        //JTextField amountField = new JTextField(10);
+        JTextField amountCorrenteField = new JTextField(10);
+        JTextField amountPoupancaField = new JTextField(20);
+
+        JLabel balanceLabelCorrente = new JLabel("Saldo Conta Corrente: " + contaCorrente.getSALDO() + "\n");
+        JLabel balanceLabelPoupanca = new JLabel("Saldo Conta Poupança: " + contaPoupanca.getSALDO() + "\n");
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
 
         // Ação para depósito na conta corrente
         depositCorrenteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
                 double amount = Double.parseDouble(amountCorrenteField.getText());
                 contaCorrente.depositar(amount);
                 balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSaldo());
+=======
+
+                double amount = Double.parseDouble(amountCorrenteField.getText());
+
+                //double amount = Double.parseDouble(amountCorrenteField.getText());
+                contaCorrente.DEPOSITAR(amount);
+                balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSALDO());
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
             }
         });
 
@@ -49,9 +76,20 @@ public class TesteBanco {
         withdrawCorrenteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
+<<<<<<< HEAD
                     double amount = Double.parseDouble(amountCorrenteField.getText());
                     contaCorrente.sacar(amount);
                     balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSaldo());
+=======
+
+                    double amount = Double.parseDouble(amountCorrenteField.getText());
+                    contaCorrente.SACAR(amount);
+                    balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSALDO());
+
+                  //  double amount = Double.parseDouble(amountCorrenteField.getText());
+                  //  contaCorrente.SACAR(amount);
+                  //  balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSALDO());
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
                 } catch (SaldoInsuficienteException ex) {
                     JOptionPane.showMessageDialog(frame, ex.getMessage());
                 }
@@ -93,15 +131,25 @@ public class TesteBanco {
                 }
             }
         });
+<<<<<<< HEAD
 
         // Ação para transferir Corrente para Conta (pendente)
+=======
+        // Ação para transferir Corrente para Poupança (pendente)
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
         TransferirCorrentePoupancaButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     double amount = Double.parseDouble(amountCorrenteField.getText());
+<<<<<<< HEAD
                     contaCorrente.transferir(contaPoupanca, amount);
                     balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSaldo());
                     balanceLabelPoupanca.setText("Saldo Conta Poupança: " + contaPoupanca.getSaldo());
+=======
+                    contaCorrente.TRANSFERIR(contaPoupanca, amount);
+                    balanceLabelCorrente.setText("Saldo Conta Corrente: " + contaCorrente.getSALDO());
+                    balanceLabelPoupanca.setText("Saldo Conta Poupança: " + contaPoupanca.getSALDO());
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
                 } catch (SaldoInsuficienteException ex) {
                     JOptionPane.showMessageDialog(frame, ex.getMessage());
                 }
@@ -117,6 +165,7 @@ public class TesteBanco {
         });
 
         // Adiciona componentes ao frame
+<<<<<<< HEAD
         frame.add(new JLabel("Valor Corrente:"));
         frame.add(amountCorrenteField);
         frame.add(depositCorrenteButton);
@@ -125,10 +174,24 @@ public class TesteBanco {
         frame.add(balanceLabelCorrente);
 
         frame.add(new JLabel("Valor Poupança:"));
+=======
+        frame.add(new JLabel("Valor Conta Corrente:"));
+        frame.add(amountCorrenteField);
+        frame.add(depositButton);
+        frame.add(withdrawButton);
+        frame.add(TransferirCorrentePoupancaButton);
+        frame.add(balanceLabelCorrente);
+
+        frame.add(new JLabel("Valor Conta Poupança: "));
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
         frame.add(amountPoupancaField);
         frame.add(depositPoupancaButton);
         frame.add(withdrawPoupancaButton);
         frame.add(TransferirPoupancaCorrenteButton);
+<<<<<<< HEAD
+=======
+        frame.add(rendimentoButton);
+>>>>>>> 53c6ece (Mexi em algumas coisas aqui.)
         frame.add(balanceLabelPoupanca);
 
         frame.add(rendimentoButton);
